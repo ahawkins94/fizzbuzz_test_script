6ef50d79d3e1b915cda935effb9ce424706f6440
ahawkins94 <adamhawkins94@hotmail.co.uk>
2017-10-17 23:27:21 +0100

    commented out the first test and made sure it failed, added the extra code that changes everything divisible by 3 into a fizz and passed the test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,9 +1,13 @@
 require_relative 'spec_helper'
 
 describe FizzBuzz do
-  # it 'should print Fizz instead of multiples of 3'
-  #   game = Fizzbuzz.new
-  # end
+  it 'should print Fizz instead of multiples of 3' do
+    game = FizzBuzz.new
+
+    result = game.sequence
+
+    expect(result[2]).to eq('Fizz')
+  end
 
   it 'should generate numbers from 1 to 100' do
     game = FizzBuzz.new
