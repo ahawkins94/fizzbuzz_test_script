abe8c4cbf4fc212b88db4d09d638c99de4c7681e
ahawkins94 <adamhawkins94@hotmail.co.uk>
2017-10-17 22:40:47 +0100

    adding tests before code

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,1 +1,15 @@
 require_relative 'spec_helper'
+
+describe Fizzbuzz do
+  # it 'should print Fizz instead of multiples of 3'
+  #   game = Fizzbuzz.new
+  # end
+
+  it 'should generate numbers from 1 to 100' do
+    game = FizzBuzz.new
+
+    result = game.numbers
+
+    expect(result).to eq((1..100).to_a)
+  end
+end
