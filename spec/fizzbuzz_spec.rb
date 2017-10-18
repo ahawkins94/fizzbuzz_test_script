bbb9346134f5397b0268e20c006d4c7945aca933
ahawkins94 <adamhawkins94@hotmail.co.uk>
2017-10-18 11:07:19 +0100

    dried up the code a bit

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,27 +1,25 @@
 require_relative 'spec_helper'
 
 describe FizzBuzz do
+
+  before(:each) do
+    @game = FizzBuzz.new
+    @result = @game.sequence
+  end
+
   it 'should print Fizz instead of multiples of 3' do
-    game = FizzBuzz.new
-
-    result = game.sequence
-
-    expect(result[2]).to eq('Fizz')
+    expect(@result[2]).to eq('Fizz')
   end
 
   it 'should print Buzz instead of multiples of 5' do
-    game = FizzBuzz.new
-
-    result = game.sequence
-
-    expect(result[4]).to eq('Buzz')
+    expect(@result[4]).to eq('Buzz')
   end
 
-  it 'should generate numbers from 1 to 100' do
-    game = FizzBuzz.new
+  it 'should replace multiples of both 3 and 5 with FizzBuzz' do
+    expect(@result[14]).to eq('FizzBuzz')
+  end
 
-    result = game.numbers
-
-    expect(result).to eq((1..100).to_a)
+  it 'should print the last number, 100, as Buzz' do
+    expect(@result[-1]).to eq('Buzz')
   end
 end
